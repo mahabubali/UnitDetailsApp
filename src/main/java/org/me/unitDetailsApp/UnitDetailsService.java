d3227d9d6fea2e9cb9b0fb4b4d1f5a25b91fc3d3
mahabubali <mahabubali@localhost>
2025-06-15 14:28:42 +0530

    testing the change for Dev branch

--- src/main/java/org/me/unitDetailsApp/UnitDetailsService.java
+++ src/main/java/org/me/unitDetailsApp/UnitDetailsService.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class UnitDetailsService {
 
 	public static void main(String[] args) {
+		System.out.println("Hello from Spring boot3!");
 		SpringApplication.run(UnitDetailsService.class, args);
 	}
 }
